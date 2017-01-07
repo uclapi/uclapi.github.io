c0673df0a14252b2f3cc6e41133d76f27ad4043a
Christopher Hammond <christopherhammond13@gmail.com>
2017-01-07 00:48:13 +0000

    Added more messages to the build.sh

--- build.sh
+++ build.sh
@@ -3,15 +3,18 @@
 # Build all the files
 
 # Build the site with gulp
+echo "Building the main site..."
 cd site
 gulp build
 cd ..
 
 #Build the docs
+echo "Building the docs site..."
 cd docs
 bundle exec middleman build --clean
 cd ..
 
+echo "Organising files..."
 cp -r ./docs/build/* ./
 mv index.html docs.html
 cp ./site/statics/stylesheets/* ./stylesheets/
